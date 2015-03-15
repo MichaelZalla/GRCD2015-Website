0cc5538a8eba9cb125e730a6ebf992b572601d15
Michael Zalla <michael.zalla@gmail.com>
2015-03-14 21:15:35 -0400

    Stage 2 prototype features;

--- src/js/grcd-main.js
+++ src/js/grcd-main.js
@@ -4,39 +4,46 @@
 				
 		// Set up an event listener for menu screen transitions
 		// Parent element wraps the hamburger menu's pseudo-elements
-		$('.ss-nav-menu-toggle').parent().on('mouseup', function(e) {
-			$('.ss-nav-menu').toggleClass('expanded');
+		$('.ss-header-navigation-toggle').parent().on('mouseup', function(e) {
+			$('.ss-header-navigation').toggleClass('expanded');
 		});
 
 		// ScrollIt
 		$.scrollIt({
 			easing: 'swing',
-			scrollTime: 1095,
-			activeClass: 'active'
-			// ,topOffset: -68
+			scrollTime: 850,
+			activeClass: 'active',
+			topOffset: -1 * $('#header').outerHeight()
 		});
 
 		// Letter-grid (dynamic position)
 
 		var logo = $('#branding').find('.logo');
 		var grid = $('#branding').find('.letter-grid');
+		
+		// Project preview wallapper (dynamic position)
+
+		var projectPreview = $('.project-preview')
+		var projectPreviewImage = projectPreview.find('.preview-image');
 
 		function onResizeWindow(e) {
 
-			var o = logo.offset();
+			var lo = logo.offset(),
+				po = projectPreviewImage.offset();
 
 			grid.css({
-				'left': o.left - 274,
-				'top': o.top - 210
-				// 'left': o.left - 220,
-				// 'top': o.top - 240
+				'left': lo.left - 274,
+				'top': lo.top - 210
+			});
+
+			projectPreview.css({
+				'background-position-x': po.left
 			});
 
 		}
 
-
 		$(window).on('resize', onResizeWindow);
-
+		
 		onResizeWindow();
 
 	});
